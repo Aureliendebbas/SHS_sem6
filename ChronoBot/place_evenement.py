e6d849e426148e321ab96c21db7ed82378c08ea1
Aurelien <aurelien.balice@epfl.ch>
2018-05-08 12:12:50 +0200

    Final commit

--- ChronoBot/place_evenement.py
+++
@@ -1,4 +0,0 @@
-from retrieve_content import retrieve_content
-def place_evenement(annee,evenement): #format annee: 'aaaa'
-                                    #format dans wikipast: '*[[aaaa.mm.jj]] (...)' seulement le début du format nous intéress
-    return retrieve_content(annee)+'\n'+evenement
